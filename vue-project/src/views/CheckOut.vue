dd79de897f4569035d77dcfb8acbf13d24a20387
Nelskika <55363532+Nelskika@users.noreply.github.com>
2022-04-11 23:32:12 -0400

    added cart

--- vue-project/src/views/CheckOut.vue
+++ vue-project/src/views/CheckOut.vue
@@ -6,26 +6,6 @@
         
         <table>
             <td>
-<<<<<<< HEAD
-                <tr>Name : <td> 
-                    <input type="text" min ="" v-model.lazy="custName"> </td> 
-                    
-                    </tr>
-                <tr>Street Address : <td> 
-                    <input type="text" min ="" v-model.lazy="custAddress"> 
-                    </td>
-                    </tr>
-                <tr>City : 
-                    <td>
-                        <input type="text" min ="" v-model.lazy="custCity"> 
-                        </td> 
-                </tr>
-                <tr>Zipcode :
-                    <td>
-                        <input type="text" min ="" v-model.lazy="custZip">
-                    </td>
-                     </tr>
-=======
                 <tr>Name : </tr>
                 <tr>Street Address : </tr>
                 <tr>City : </tr>
@@ -36,37 +16,23 @@
                 <tr><input type="text" v-model.lazy="custAddress"/></tr>
                 <tr><input type="text" v-model.lazy="custCity"/></tr>
                 <tr><input type="text" v-model.lazy="custZip"/></tr>
->>>>>>> 1727a2af688f44829f2d739353be2060f7cc17c1
             </td>
         </table>
-<<<<<<< HEAD
 
         <table>
-        <th>Product</th>
-        <th>Price</th>
-        <th>Quantity</th>
-        <tr v-for="(item, pos) in items" :key="pos">
-            <td>{{items[pos].name}}</td>
-            <td>{{items[pos].price}} </td>
-            <td>{{items[pos].quantity}} </td>
-        </tr>        
-        <tr>Total :
-        <td> ${{this.total}} </td>    
-        </tr>
-        </table>
-        
-        </div>
-        <h2>Payment Information</h2>
-        <table>
+            <th>Product</th>
+            <th>Quantity</th>
+            <th>Price</th>
+            <tr v-for="(item,pos) in items" :key =pos> 
+                <td>{{item.name}}</td>
+                <td>{{item.quantity}}</td>
+                <td>{{item.price}}</td>
+
             
-                <tr>Credit Card number :
-                    <td>
-                    <input type="text" min ="" v-model.number="cardNum">
-                    </td>
-                         </tr>
-                <tr>Expiration Date : 
-                    <td>
-=======
+            
+            </tr>
+
+        </table>
         <h3>    Total : ${{this.total}}</h3>
         </div>
         <h2>Payment Information</h2>
@@ -82,35 +48,17 @@
                     <input type="text" min ="" v-model.lazy="cardNum">
                 </tr>
                 <tr>
->>>>>>> 1727a2af688f44829f2d739353be2060f7cc17c1
                     <input type="text" min ="" v-model.lazy="cardExp">
-
-                    </td>
                 </tr>
-<<<<<<< HEAD
-                <tr>CVV2 Number :
-                    <td>
-                    <input type="text" min ="" v-model.number="cardCv">
-                        
-                    </td>
-                     </tr>
-                <tr>Name on card : 
-                    <td>
-=======
                 <tr>
                     <input type="text" min ="" v-model.lazy="cardCVV">
                 </tr>
                 <tr>
->>>>>>> 1727a2af688f44829f2d739353be2060f7cc17c1
                     <input type="text" min ="" v-model.lazy="cardName">
-                    </td>
-                     </tr>
+                </tr>
+            </td>
         </table>
-<<<<<<< HEAD
-        <button>Purchase</button>
-=======
         <button @click="save()">Submit</button>
->>>>>>> 1727a2af688f44829f2d739353be2060f7cc17c1
         </div>
 </template>
 
@@ -120,6 +68,12 @@ import { collection, getDocs, QuerySnapshot, QueryDocumentSnapshot, addDoc, dele
 import { db } from '../firebase';
 import { getAuth } from 'firebase/auth';
 
+type Item = {
+    name: "",
+    price:0,
+    quantity:0
+
+}
 export default class CheckoutView extends Vue {
     private prices: Array<number> = [];
     private products = {};
@@ -133,18 +87,18 @@ export default class CheckoutView extends Vue {
     cardCVV = "";
     cardName = "";
     uid = "";
-
+    private items : Array<Item> = [];
     save():void{  
         const orderRef = collection(db, "Users", this.uid + "", "Orders");
         const cartRef = collection(db, "Users", this.uid + "", "Cart" );
-
         addDoc(orderRef, {name: this.custName, address: this.custAddress,
         city: this.custCity, zip: this.custZip, cardNumber: this.cardNum,
         cardExpiration: this.cardExp, cardCVV2: this.cardCVV, NameOnCard: this.cardName});
-
         getDocs(cartRef).then((qs:QuerySnapshot) => {
             qs.forEach((qd: QueryDocumentSnapshot) => {
                 deleteDoc(doc(db, "Users", this.uid + "", "Cart", "" + qd.id));
+                
+            
             })
         })
             .catch((error) => {
@@ -152,7 +106,6 @@ export default class CheckoutView extends Vue {
             });
             this.$router.replace("/about");
     }
-
     mounted() {
         const auth = getAuth();
         if(auth.currentUser != null){
@@ -163,7 +116,9 @@ export default class CheckoutView extends Vue {
         getDocs(collectionRef).then((qs:QuerySnapshot) => {
                 
             qs.forEach((qd: QueryDocumentSnapshot) => {
-                this.total +=qd.data().price;
+                this.total +=qd.data().price * qd.data().quantity;
+
+                this.items.push({name:qd.data().productName, price: qd.data().price, quantity: qd.data().quantity});
                 })
             })
             .catch((error) => {
@@ -204,66 +159,4 @@ export default class CheckoutView extends Vue {
             grid-template-columns: 32% 32% 32%;
         }
     }
-<<<<<<< HEAD
 </style>
-
-<script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import ProductTile from '../components/ProductTile.vue';
-import { collection, getDocs, QuerySnapshot, DocumentSnapshot, QueryDocumentSnapshot } from 'firebase/firestore';
-import { db } from '../firebase';
-import { getAuth } from 'firebase/auth';
-import { onMounted } from '@vue/runtime-core';
-/* @Options({
-  components: {
-    ProductTile,
-  },
-}) */
-
-type Item = {
-        price: 0;
-        name: 0;
-        quantity :0;
-    }
-
-    
-
-export default class CheckoutView extends Vue {
-    private prices: Array<number> = [];
-    public total = 0;
-
-    
-    private items : Array<Item> = [];
-    
-    mounted() {
-        let uid = null;
-        const auth = getAuth();
-        if(auth.currentUser != null){
-            uid= auth.currentUser.uid;
-        }
-        //const productCollection = collection(db, "products");
-        const collectionRef = collection(db, "Users", uid + "", "Cart" );
-        getDocs(collectionRef).then((qs:QuerySnapshot) => {
-            qs.forEach((qd: QueryDocumentSnapshot) => {
-                this.total +=qd.data().price * qd.data().quantity;
-                console.log(qd.data().price)
-
-                let item : Item ={
-                    name : qd.data().productName,
-                    price : qd.data().price,
-                    quantity : qd.data().quantity
-                }
-
-                this.items.push(item);
-                })
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-        console.log("after prices load");
-    }
-}
-</script>
-=======
-</style>
->>>>>>> 1727a2af688f44829f2d739353be2060f7cc17c1
